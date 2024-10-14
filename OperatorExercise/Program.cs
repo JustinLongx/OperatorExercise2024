92d5a8b7fc8bbad1a7db8d1ed1ab3c691003ec1c
justin long <justinxlong93@gmail.com>
2024-10-14 16:19:50 -0500

    Part 1 of exercise

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace OperatorExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            var sum = a + b;
+            var subtract = a - b;
+            var multiply = a * b;
+            var division = a / b;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+
+
+            Console.WriteLine($"{a}/{b} is {division} remainder {remainder}");
+
+        }
+    }
+}
